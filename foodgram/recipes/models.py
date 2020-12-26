c7f07ac5c4fb350b2285e126bdfb39b5128a31a4
sorochinskii <sorochinsky.alex@gmail.com>
2020-12-26 18:13:20 +0500

    Models modification.

--- foodgram/recipes/models.py
+++ foodgram/recipes/models.py
@@ -1,33 +1,49 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
-    pass
-
-class Ingredients(models.Model):
-
-    name = models.CharField(_(""), max_length=200)
-    qty = models.IntegerField(_(""))
-    unit = models.ForeignKey(Units,
-                             verbose_name=_(""),
-                             on_delete=models.CASCADE
+    USER = 'user'
+    MODERATOR = 'moderator'
+    ADMIN = 'admin'
+    DJADMIN = "djadmin"
+    ROLES = (
+        (USER, 'user'),
+        (MODERATOR, 'moderator'),
+        (ADMIN, 'admin'),
+        (DJADMIN, 'djadmin'),
     )
-    
-    class Meta:
-        verbose_name = class Ingredients")
-
-    def __str__(self):
-        return self.name
+    bio = models.TextField(max_length=500, blank=True, null=True)
+    role = models.CharField(max_length=30, choices=ROLES, default='user')
+    confirmation_code = models.CharField(max_length=200, default='FOOBAR')
 
 
 class Units(models.Model):
 
-    name = models.CharField(_(""), max_length=20)
-
-    class Meta:
-        verbose_name = class Units")
-        verbose_name_plural = class Unitss")
+    name = models.CharField(max_length=20)
 
     def __str__(self):
         return self.name
 
+
+class Ingredients(models.Model):
+
+    name = models.CharField(max_length=200)
+    qty = models.IntegerField()
+    unit = models.ManyToManyField(
+                            Units,
+    )
+    
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+
+    name = models.CharField(max_length=50)
+    pic = models.ImageField(upload_to='meals/%Y-%m-%d')
+    description = models.TextField()
+    duration = models.IntegerField()
+    ingredients = models.ManyToManyField(Ingredients)
+    # tags = 
+    slug = models.SlugField()
